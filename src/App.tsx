f3ad4027ae42b0a411f9137197fabf5696fd24f7
andrescastellanos112013 <andrescastellanos112013@gmail.com>
2020-06-30 19:53:42 -0500

    [App] It was improved, and the Landing page was added

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  HashRouter,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+
+import { ThemeProvider } from 'components/ThemeContext';
+import { PathRoutes } from './constantsApp';
+import { Landing } from './pages/Landing';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider>
+      <HashRouter>
+        <Switch>
+          <Route
+            exact
+            path={PathRoutes.LANDING}
+            component={Landing}
+          />
+          <Redirect
+            exact
+            from={PathRoutes.ROOT}
+            to={PathRoutes.LANDING}
+          />
+        </Switch>
+      </HashRouter>
+    </ThemeProvider>
   );
 }
 
