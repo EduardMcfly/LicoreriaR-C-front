81e506d94b788623af71f7eeed1daa1fa7e27869
Eduard Castellanos <andrescastellanos112013@gmail.com>
2021-03-13 14:15:41 -0500

    [components/Fields] Aggregate Amount component

---
+++ src/components/Fields/Amount.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
+import FormControl from '@material-ui/core/FormControl';
+import AddIcon from '@material-ui/icons/Add';
+import RemoveIcon from '@material-ui/icons/Remove';
+import { IconButton } from '@material-ui/core';
+
+interface AmountProps {
+  value: number;
+  handleChange: (value: number) => void;
+}
+
+export const Amount = ({ value, handleChange }: AmountProps) => (
+  <FormControl fullWidth>
+    <InputLabel>Cantidad</InputLabel>
+    <Input
+      type="number"
+      value={value || ''}
+      startAdornment={
+        <IconButton
+          color="primary"
+          aria-label="reduce"
+          onClick={() => {
+            handleChange(Math.max(value - 1, 0));
+          }}
+        >
+          <RemoveIcon fontSize="small" />
+        </IconButton>
+      }
+      endAdornment={
+        <IconButton
+          color="primary"
+          aria-label="increase"
+          onClick={() => {
+            handleChange(value + 1);
+          }}
+        >
+          <AddIcon fontSize="small" />
+        </IconButton>
+      }
+      onChange={({ target: { value } }) => handleChange(+value)}
+    />
+  </FormControl>
+);
