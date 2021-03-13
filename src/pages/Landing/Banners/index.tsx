6df19f288673cae12c23ed4768c130bb6dbe124e
Eduard Castellanos <andrescastellanos112013@gmail.com>
2021-03-13 14:05:28 -0500

    [Landing/Banners] Design has been improved

--- src/pages/Landing/Banners/index.tsx
+++ src/pages/Landing/Banners/index.tsx
@@ -1,17 +1,19 @@
-import React from 'react';
 import {
   Theme,
   createStyles,
   makeStyles,
+  emphasize,
+  fade,
 } from '@material-ui/core/styles';
-import { ButtonBase, Typography } from '@material-ui/core';
+import { Typography } from '@material-ui/core';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
 import currencyFormatter from 'currency-formatter';
 
 import { getGrid, useWidth } from 'utils';
-import { bannersList } from './bannersList';
+import { products } from './products';
+import AddCart from './AddCart';
 
 const useStyles = makeStyles((theme: Theme) => {
   const { contrastText } = theme.palette.primary;
@@ -25,20 +27,31 @@ const useStyles = makeStyles((theme: Theme) => {
       color: contrastText,
     },
     gridListTile: {
+      textAlign: 'center',
       '&:hover': {
         '& $gridListTileBar': {
-          height: 0,
+          height: 120,
           overflow: 'hidden',
         },
       },
     },
     gridListTileBar: {
+      background: fade(
+        emphasize(theme.palette.background.paper, 1),
+        0.7,
+      ),
+      height: 110,
       transition: `height 0.5s ease`,
       borderRadius: theme.spacing(4, 4, 0, 0),
+      alignItems: 'start',
+    },
+    gridListTileBarTitle: {
+      marginTop: theme.spacing(1),
     },
     titleWrap: {
       color: contrastText,
     },
+    button: { margin: theme.spacing(1, 0, 1) },
   });
 });
 
@@ -55,27 +68,30 @@ export const Banners = () => {
   );
 
   return (
-    <GridList cellHeight={180} cols={cols} className={classes.root}>
-      {bannersList.map(({ title, price, image }, i) => {
+    <GridList cellHeight={300} cols={cols} className={classes.root}>
+      {products.map((product, i) => {
+        const { title, price, image } = product;
         return (
-          <GridListTile
-            component={ButtonBase}
-            key={i}
-            className={classes.gridListTile}
-          >
+          <GridListTile key={i} className={classes.gridListTile}>
             <img src={image} alt={title} />
             <GridListTileBar
               title={title}
               subtitle={
-                <Typography>
-                  {currencyFormatter.format(price, {
-                    code: 'COP',
-                    precision: 0,
-                  })}
-                </Typography>
+                <>
+                  <Typography>
+                    {currencyFormatter.format(price, {
+                      code: 'COP',
+                      precision: 0,
+                    })}
+                  </Typography>
+                  <AddCart {...{ product }} />
+                </>
               }
               className={classes.gridListTileBar}
-              classes={{ titleWrap: classes.titleWrap }}
+              classes={{
+                titleWrap: classes.titleWrap,
+                title: classes.gridListTileBarTitle,
+              }}
             />
           </GridListTile>
         );
