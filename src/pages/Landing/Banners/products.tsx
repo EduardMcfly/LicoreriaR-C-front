144c724d3634100ce820b0ba93733d735c71bdb7
Eduard Castellanos <andrescastellanos112013@gmail.com>
2021-03-13 13:57:27 -0500

    [Landing/Banners] Has been renamed the file bannersList to products

--- src/pages/Landing/Banners/products.tsx
+++ src/pages/Landing/Banners/products.tsx
@@ -1,81 +1,107 @@
-interface BannersList {
+export interface Product {
+  id: number;
   title: string;
   price: number;
   image: string;
+  max: number;
 }
 
-export const bannersList: BannersList[] = [
+export const products: Product[] = [
   {
+    id: 1,
     image:
       'https://dislicores.vteximg.com.br/arquivos/ids/155997-292-292/Licores-whisky_002320_1.jpg?v=637085599793170000',
     title: 'Whiskey Bulleit Bourbon botella 750ml',
     price: 111800,
+    max: 22,
   },
   {
+    id: 2,
     image:
       'https://dislicores.vteximg.com.br/arquivos/ids/156008-292-292/Licores-whisky_304032_1.jpg?v=637085599836030000',
     title: 'Whisky Chivas Regal 18 Años botella 700ml',
     price: 207500,
+    max: 22,
   },
   {
+    id: 3,
     image:
       'https://dislicores.vteximg.com.br/arquivos/ids/156001-292-292/Licores-whisky_002328_1.jpg?v=637085599810530000',
     title: 'Whisky Johnnie Walker Black Label botella 1000ml',
     price: 125100,
+    max: 22,
   },
   {
+    id: 4,
     image:
       'https://dislicores.vteximg.com.br/arquivos/ids/155979-292-292/Licores-whisky_002014_1.jpg?v=637085599725370000',
     title: 'Whisky Old Parr 12 Años botella1000ml',
     price: 134200,
+    max: 22,
   },
   {
+    id: 5,
     image:
       'https://dislicores.vteximg.com.br/arquivos/ids/155991-292-292/Licores-whisky_002236_1.jpg?v=637085599769870000',
     title: 'Whisky Buchanans Deluxe 12 Años botella 1000ml',
     price: 148100,
+    max: 22,
   },
   {
+    id: 6,
     image:
       'https://dislicores.vteximg.com.br/arquivos/ids/155938-292-292/Licores-ron_002191_1.jpg?v=637085599591270000',
     title: 'Ron Zacapa 23 Años botella 750ml',
     price: 153100,
+    max: 22,
   },
   {
+    id: 7,
     image:
       'https://dislicores.vteximg.com.br/arquivos/ids/155919-292-292/Licores-ginebra_002235_1.jpg?v=637085599524830000',
     title: 'Ginebra Tanqueray Rangpur botella 750ml',
     price: 121900,
+    max: 22,
   },
   {
+    id: 9,
     image:
       'https://dislicores.vteximg.com.br/arquivos/ids/155920-292-292/Licores-ginebra_002343_1.jpg?v=637085599527730000',
     title: 'Ginebra Gordons Pink botella 750ml',
     price: 59400,
+    max: 22,
   },
   {
+    id: 10,
     image:
       'https://dislicores.vteximg.com.br/arquivos/ids/155931-292-292/Licores-ginebra_954005_1.jpg?v=637085599565770000',
     title: 'Ginebra Hendricks botella 750ml',
     price: 174900,
+    max: 22,
   },
   {
+    id: 11,
     image:
       'https://dislicores.vteximg.com.br/arquivos/ids/155983-292-292/Licores-whisky_002018_1.jpg?v=637085599741530000',
     title: 'Whisky Johnnie Walker Red Label botella 700ml',
     price: 50300,
+    max: 22,
   },
   {
+    id: 12,
     image:
       'https://dislicores.vteximg.com.br/arquivos/ids/155988-292-292/Licores-whisky_002216_1.jpg?v=637085599759700000',
     title: 'Whisky Jhonnie Walker Double Black botella 700ml',
     price: 123700,
+    max: 22,
   },
   {
+    id: 13,
     image:
       'https://dislicores.vteximg.com.br/arquivos/ids/156781-292-292/Licores-whisky_002361_1.jpg?v=637187643607900000',
     title:
       'Whisky Johnnie Walker Dragon Glass a Song of Fire botella',
     price: 120600,
+    max: 22,
   },
 ];
