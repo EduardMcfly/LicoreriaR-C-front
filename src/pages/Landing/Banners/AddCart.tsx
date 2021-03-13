0c1bb6091f5d063d566f6f5ec1940b4522cee492
Eduard Castellanos <andrescastellanos112013@gmail.com>
2021-03-13 14:16:40 -0500

    chore: The AddCart component has been refactored

--- src/pages/Landing/Banners/AddCart.tsx
+++ src/pages/Landing/Banners/AddCart.tsx
@@ -1,42 +1,25 @@
 import React from 'react';
-import {
-  createStyles,
-  makeStyles,
-  Theme,
-} from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
-import InputLabel from '@material-ui/core/InputLabel';
-import Input from '@material-ui/core/Input';
-import FormControl from '@material-ui/core/FormControl';
-import AddIcon from '@material-ui/icons/Add';
-import RemoveIcon from '@material-ui/icons/Remove';
 import {
   Avatar,
   ListItem,
   ListItemAvatar,
   ListItemText,
-  IconButton,
 } from '@material-ui/core';
 
 import { useShop } from 'contexts/Shop';
 import { Product } from './products';
-
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    formControl: {},
-  }),
-);
+import { Amount } from 'components/Fields/Amount';
 
 interface AddCartProps {
   product: Product;
 }
 
 export default function AddCart({ product: item }: AddCartProps) {
-  const classes = useStyles();
   const [open, setOpen] = React.useState(false);
   const [amount, setAmount] = React.useState<number>(0);
   const { addItem, items } = useShop();
@@ -81,42 +64,7 @@ export default function AddCart({ product: item }: AddCartProps) {
           </ListItem>
         </DialogTitle>
         <DialogContent>
-          <FormControl className={classes.formControl} fullWidth>
-            <InputLabel id="add-cart-dialog-select-label">
-              Cantidad
-            </InputLabel>
-            <Input
-              type="number"
-              id="my-input"
-              aria-describedby="my-helper-text"
-              value={amount || ''}
-              startAdornment={
-                <IconButton
-                  color="primary"
-                  aria-label="reduce"
-                  onClick={() => {
-                    handleChange(Math.max(amount - 1, 0));
-                  }}
-                >
-                  <RemoveIcon fontSize="small" />
-                </IconButton>
-              }
-              endAdornment={
-                <IconButton
-                  color="primary"
-                  aria-label="increase"
-                  onClick={() => {
-                    handleChange(amount + 1);
-                  }}
-                >
-                  <AddIcon fontSize="small" />
-                </IconButton>
-              }
-              onChange={({ target: { value } }) =>
-                handleChange(value)
-              }
-            />
-          </FormControl>
+          <Amount handleChange={handleChange} value={amount} />
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose} color="primary">
