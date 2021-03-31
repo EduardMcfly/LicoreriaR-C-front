3945d785d2f73cbed8f5eb2e99d4ab870397e010
Eduard Castellanos <andrescastellanos112013@gmail.com>
2021-03-30 22:58:46 -0500

    chore(App): ApolloProvider has been added

--- src/App.tsx
+++ src/App.tsx
@@ -6,33 +6,37 @@ import {
   Redirect,
 } from 'react-router-dom';
 import { CssBaseline } from '@material-ui/core';
+import { ApolloProvider } from '@apollo/client';
 
 import { ThemeProvider } from 'components/ThemeContext';
 import { PathRoutes } from './constantsApp';
 import { Landing } from './pages/Landing';
 import { ShopProvider } from './contexts/Shop';
+import { client } from './client';
 
 function App() {
   return (
-    <ThemeProvider>
-      <CssBaseline />
-      <ShopProvider>
-        <HashRouter>
-          <Switch>
-            <Route
-              exact
-              path={PathRoutes.LANDING}
-              component={Landing}
-            />
-            <Redirect
-              exact
-              from={PathRoutes.ROOT}
-              to={PathRoutes.LANDING}
-            />
-          </Switch>
-        </HashRouter>
-      </ShopProvider>
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <ThemeProvider>
+        <CssBaseline />
+        <ShopProvider>
+          <HashRouter>
+            <Switch>
+              <Route
+                exact
+                path={PathRoutes.LANDING}
+                component={Landing}
+              />
+              <Redirect
+                exact
+                from={PathRoutes.ROOT}
+                to={PathRoutes.LANDING}
+              />
+            </Switch>
+          </HashRouter>
+        </ShopProvider>
+      </ThemeProvider>
+    </ApolloProvider>
   );
 }
 
