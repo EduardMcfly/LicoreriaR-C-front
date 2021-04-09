552c92a3c6fad301a2cdd7b51a2c1162ddce5bce
Eduard Castellanos <andrescastellanos112013@gmail.com>
2021-04-09 09:01:48 -0500

    chore(constantsApp): createAPIImageRoute has been added

--- src/constantsApp/index.ts
+++ src/constantsApp/index.ts
@@ -1,6 +1,40 @@
+import { stringify, ParsedUrlQueryInput } from 'querystring';
+
 export const AppName = process.env.REACT_APP_NAME;
 
 export enum PathRoutes {
   LANDING = '/landing',
   ROOT = '/',
 }
+
+type Format =
+  | 'heic'
+  | 'heif'
+  | 'jpeg'
+  | 'jpg'
+  | 'png'
+  | 'raw'
+  | 'tiff'
+  | 'webp';
+
+interface PropsResize extends ParsedUrlQueryInput {
+  format?: Format;
+  width?: number;
+  height?: number;
+}
+
+const { REACT_APP_IMAGE } = process.env;
+
+export const createAPIImageRoute = (
+  path: string,
+  { width = 300, ...rest }: PropsResize = {},
+): string => {
+  /**
+   * if exist in path remove ?
+   **/
+  path = path.replace(/\?$/gm, '');
+  return `${REACT_APP_IMAGE}?file=${path}&${stringify({
+    width,
+    ...rest,
+  })}`;
+};
