81217794f2823516a46438f0eb4875eac1a68f30
Eduard Castellanos <andrescastellanos112013@gmail.com>
2021-04-09 09:17:14 -0500

    chore(client): createUploadLink is used

--- src/client.ts
+++ src/client.ts
@@ -1,6 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { createUploadLink } from 'apollo-upload-client';
 
 export const client = new ApolloClient({
-  uri: process.env.REACT_APP_API,
   cache: new InMemoryCache(),
+  link: createUploadLink({ uri: process.env.REACT_APP_API }),
 });
