979cc3aeba35fd88704ef21282cce38e9d3beb50
Eduard Castellanos <andrescastellanos112013@gmail.com>
2021-04-15 12:49:21 -0500

    chore(graphqlAPI): useCreateProduct has been improved

--- src/graphqlAPI/products/index.ts
+++ src/graphqlAPI/products/index.ts
@@ -21,17 +21,20 @@ export const PRODUCTS_QUERY = gql`
 export const useProducts = () => {
   return useQuery<TDataProducts>(PRODUCTS_QUERY);
 };
+export const CREATE_PRODUCT_MUTATION = gql`
+  mutation($product: ProductInput!) {
+    createProduct(product: $product) {
+      id
+      name
+    }
+  }
+`;
 
 export const useCreateProduct = () => {
   return useMutation<
     Pick<Mutation, 'createProduct'>,
     MutationCreateProductArgs
-  >(gql`
-    mutation($product: ProductInput!) {
-      createProduct(product: $product) {
-        id
-        name
-      }
-    }
-  `);
+  >(CREATE_PRODUCT_MUTATION, {
+    refetchQueries: [{ query: PRODUCTS_QUERY }],
+  });
 };
