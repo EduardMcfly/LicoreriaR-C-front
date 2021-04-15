7bed37864d256d15c7bdbca36549e4879c91c7de
Eduard Castellanos <andrescastellanos112013@gmail.com>
2021-04-15 09:33:05 -0500

    chore: NumberFormatCustom component has been added

---
+++ src/components/NumberFormatCustom/index.tsx
@@ -0,0 +1,33 @@
+import NumberFormat from 'react-number-format';
+
+export interface NumberFormatCustomProps {
+  inputRef: (instance: NumberFormat | null) => void;
+  onChange: (event: {
+    target: { name: string; value: string };
+  }) => void;
+  name: string;
+}
+
+export function NumberFormatCustom(props: NumberFormatCustomProps) {
+  const { inputRef, onChange, ...other } = props;
+
+  return (
+    <NumberFormat
+      {...other}
+      getInputRef={inputRef}
+      onValueChange={(values) => {
+        onChange({
+          target: {
+            name: props.name,
+            value: values.value,
+          },
+        });
+      }}
+      thousandSeparator
+      isNumericString
+      prefix="$"
+    />
+  );
+}
+
+export default NumberFormatCustom;
