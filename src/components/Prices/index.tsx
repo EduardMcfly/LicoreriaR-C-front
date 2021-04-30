3b05af82c4dcc9583deeaf26ce8d38b4b6d2d3cb
Eduard Castellanos <andrescastellanos112013@gmail.com>
2021-04-29 20:12:54 -0500

    chore: Prices component added

---
+++ src/components/Prices/index.tsx
@@ -0,0 +1,44 @@
+import { Grid, GridSize } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core';
+
+import { UnitPrice } from './UnitPrice';
+import { TotalPrice } from './TotalPrice';
+import {
+  Breakpoint,
+  keys,
+} from '@material-ui/core/styles/createBreakpoints';
+
+type Breakpoints = Partial<Record<Breakpoint, boolean | GridSize>>;
+
+interface PricesProps extends Breakpoints {
+  amount: number;
+  price: number;
+}
+
+export const useStyles = makeStyles(() => ({
+  textCenter: {
+    textAlign: 'center',
+  },
+}));
+
+export function Prices({ amount, price, ...rest }: PricesProps) {
+  const classes = useStyles();
+  const breakpoints = keys.reduce<Breakpoints>(
+    (p, c) => ({ ...p, [c]: rest[c] }),
+    {},
+  );
+  return (
+    <>
+      <Grid item {...breakpoints}>
+        <UnitPrice value={price} className={classes.textCenter} />
+      </Grid>
+      <Grid item {...breakpoints}>
+        <TotalPrice
+          amount={amount}
+          price={price}
+          className={classes.textCenter}
+        />
+      </Grid>
+    </>
+  );
+}
