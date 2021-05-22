3df53e7c282cd19e57b04d06ae2e393a64bc5bf6
Eduard Castellanos <andrescastellanos112013@gmail.com>
2021-05-21 21:36:05 -0500

    chore(Landing/Banners): Added Products component

---
+++ src/pages/Landing/Banners/Products.tsx
@@ -0,0 +1,19 @@
+import { TDataProducts } from 'graphqlAPI';
+import Product from './Product';
+
+interface ProductsProps {
+  data?: TDataProducts;
+}
+
+export const Products = ({ data }: ProductsProps) => {
+  const products = data?.products.data;
+  return (
+    <>
+      {products?.map((product) => (
+        <Product key={product.id} product={product} />
+      ))}
+    </>
+  );
+};
+
+export default Products;
