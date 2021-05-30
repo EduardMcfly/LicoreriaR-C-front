57a39e0ea3820e9df755abad64cfe569ecbc678d
Eduard Castellanos <andrescastellanos112013@gmail.com>
2021-05-30 10:26:55 -0500

    chore(graphqlAPI/products): The productFragment have been added

---
+++ src/graphqlAPI/products/fragments.ts
@@ -0,0 +1,12 @@
+import { gql } from '@apollo/client';
+
+export const productFragment = gql`
+  fragment ProductFragment on Product {
+    id
+    name
+    image
+    description
+    price
+    amount
+  }
+`;
