9361bb8d3edc0782b8bae4a23fdbede2dc573831
Eduard Castellanos <andrescastellanos112013@gmail.com>
2021-06-05 16:10:31 -0500

    chore(Cart/steps): common file added

---
+++ src/components/Cart/steps/common.tsx
@@ -0,0 +1,27 @@
+import qs from 'querystring';
+
+import { useShop } from 'contexts';
+
+export const useOnBuy = () => {
+  const { products } = useShop();
+  return () => {
+    let text = `Hola\n`;
+    text += `Estoy interad@ en comprar estos productos:\n`;
+    for (const { id, amount } of products) {
+      const cartProduct = products.find((x) => x.id === id);
+      if (cartProduct) {
+        const { product } = cartProduct;
+        text += `${product.name}: ${amount}\n`;
+      }
+    }
+    let url = 'https://wa.me/573204283576?';
+    url += qs.stringify({ text });
+    window.location.href = url;
+  };
+};
+
+export enum Go {
+  back = 'back',
+  next = 'next',
+  buy = 'buy',
+}
