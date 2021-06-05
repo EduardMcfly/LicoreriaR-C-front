139f1f77d38a20cb58363e026bc297a9885e8f7a
Eduard Castellanos <andrescastellanos112013@gmail.com>
2021-06-05 10:22:22 -0500

    chore(contexts/Shop): Context is improved

--- src/contexts/Shop/index.tsx
+++ src/contexts/Shop/index.tsx
@@ -1,20 +1,27 @@
 import React from 'react';
 
 import { Product } from 'graphqlAPI';
+import { useProducts } from 'contexts/Products';
+import { useCartProducts } from '../../graphqlAPI/cart';
 import { getValidAmount } from './utils';
 
-type Item = Pick<Product, 'id'> & {
+interface CartProductBase {
+  id: Product['id'];
   amount: number;
-};
+}
+
+export interface CartProduct extends CartProductBase {
+  product: Product;
+}
 
 interface ShopPropsBase {}
 
 interface ShopProps extends ShopPropsBase {
-  products: Item[];
-  setProducts: React.Dispatch<React.SetStateAction<Item[]>>;
-  addProduct: (item: Item) => void;
-  changeAmount: (item: Item) => void;
-  removeProduct: (item: Item['id']) => void;
+  products: CartProduct[];
+  addProduct: (item: CartProductBase) => void;
+  changeAmount: (item: CartProductBase) => void;
+  removeProduct: (item: CartProduct['id']) => void;
+  loading: boolean;
 }
 
 const key = 'products';
@@ -25,7 +32,7 @@ const ShopContext = React.createContext<ShopProps>(
 
 const getStorage = () => {
   try {
-    const products: Item[] = JSON.parse(
+    const products: CartProductBase[] = JSON.parse(
       localStorage.getItem(key) || '',
     );
     return products;
@@ -35,60 +42,130 @@ const getStorage = () => {
 };
 
 export * from './utils';
+
+const array: Product[] = [];
+
 export const ShopProvider = ({
   children,
 }: React.PropsWithChildren<
   ShopPropsBase & { isCreating?: boolean }
 >) => {
-  const [products, setProducts] = React.useState<Item[]>(getStorage);
+  const [products, setProducts] = React.useState(
+    () => new Map<CartProduct['id'], CartProduct>(),
+  );
 
-  const saveStorage = React.useCallback(() => {
-    localStorage.setItem(key, JSON.stringify(products));
-  }, [products]);
+  const productsBase = useProducts();
+
+  const productsLoaded = productsBase.data?.products.data;
+
+  const productsStorage = getStorage();
+
+  const productsNotExist = productsStorage
+    .map(({ id }) => id)
+    .filter(
+      (id) => !productsLoaded?.some((product) => product.id === id),
+    );
+
+  const cartProducts = useCartProducts({
+    variables: {
+      products: productsNotExist,
+    },
+    skip: !!productsBase.loading || !productsNotExist.length,
+  });
+
+  const loading = productsBase.loading || cartProducts.loading;
+
+  const cartProductsData = cartProducts.data?.cartProducts;
+  const allProducts = React.useMemo(() => {
+    if (loading) return array;
+    return [...(productsLoaded || []), ...(cartProductsData || [])];
+  }, [productsLoaded, cartProductsData, loading]);
+
+  const getProduct = React.useCallback(
+    (id: CartProduct['id']) =>
+      allProducts.find((product) => product.id === id),
+    [allProducts],
+  );
 
   React.useEffect(() => {
+    let newProduct = false;
+    for (const productStorage of getStorage()) {
+      if (!products.has(productStorage.id)) {
+        const product = getProduct(productStorage.id);
+        if (product) {
+          newProduct = true;
+          products.set(product.id, {
+            id: product.id,
+            amount: productStorage.amount,
+            product,
+          });
+        }
+      }
+    }
+    if (newProduct) setProducts(new Map(products));
+  }, [products, getProduct]);
+
+  const getProducts = React.useCallback(
+    () => Array.from(products.values()),
+    [products],
+  );
+
+  const saveStorage = React.useCallback(() => {
+    localStorage.setItem(
+      key,
+      JSON.stringify(
+        getProducts().map<CartProductBase>(({ id, amount }) => ({
+          id,
+          amount,
+        })),
+      ),
+    );
+  }, [getProducts]);
+
+  const addProduct = (item: CartProductBase) => {
+    const { id, amount } = item;
+    const product = getProduct(id);
+    if (product) {
+      const shopProduct = products.get(id);
+      products.set(id, {
+        ...item,
+        amount:
+          ((shopProduct && shopProduct.amount) || 0) +
+          getValidAmount(amount),
+        product,
+      });
+      setProducts(new Map(products));
+      saveStorage();
+    }
+  };
+
+  const changeAmount = (item: CartProductBase) => {
+    const { id, amount } = item;
+    const product = products.get(id);
+    if (product) {
+      products.set(id, {
+        ...product,
+        amount: getValidAmount(amount),
+      });
+      setProducts(new Map(products));
+      saveStorage();
+    }
+  };
+
+  const removeProduct = (id: CartProduct['id']) => {
+    products.delete(id);
+    setProducts(new Map(products));
     saveStorage();
-  }, [saveStorage]);
-
-  const addProduct = (item: Item) => {
-    const newItems = [item, ...products].reduce<Record<string, Item>>(
-      (previousValue, currentValue) => {
-        const { id, amount } = currentValue;
-        if (previousValue[id])
-          previousValue[id].amount += getValidAmount(amount);
-        else previousValue[id] = currentValue;
-        return previousValue;
-      },
-      {},
-    );
-    setProducts(Object.values(newItems));
-  };
-
-  const changeAmount = (item: Item) => {
-    const { amount } = item;
-    const newProducts = products.map((product): Item => {
-      const { id } = product;
-      if (item.id === id)
-        return { id, amount: getValidAmount(amount) };
-      else return product;
-    });
-    setProducts(newProducts);
-  };
-  const removeProduct = (id: Item['id']) => {
-    const newProducts = products.filter(
-      (product) => product.id !== id,
-    );
-    setProducts(newProducts);
   };
 
   return (
     <ShopContext.Provider
       value={{
-        products,
-        setProducts,
+        products: getProducts(),
         addProduct,
         changeAmount,
         removeProduct,
+        loading,
       }}
     >
       {children}
