38f8ba95da1c03232b05f1a10e11a27128a51cd2
Eduard Castellanos <andrescastellanos112013@gmail.com>
2021-06-08 17:10:04 -0500

    chore: capacitor.config.ts file added

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ryc.app',
+  appName: 'licoreriaryc-front',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
