edd923a003603340c17eb5c0b7b8b890e9ef3801
Eduard Castellanos <andrescastellanos112013@gmail.com>
2021-06-08 23:39:55 -0500

    chore(capacitor.config): appName updated

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.ryc.app',
-  appName: 'licoreriaryc-front',
+  appName: 'licoreriaryc',
   webDir: 'build',
   bundledWebRuntime: false,
   server: {
