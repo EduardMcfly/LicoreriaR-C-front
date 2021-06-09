b086cb465aec0c262dee598154a423a05c866d4f
Eduard Castellanos <andrescastellanos112013@gmail.com>
2021-06-08 23:31:28 -0500

    chore: capacitor.config.ts file updated

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,11 @@ const config: CapacitorConfig = {
   appId: 'com.ryc.app',
   appName: 'licoreriaryc-front',
   webDir: 'build',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  server: {
+    hostname: 'www.licoreriaryc.com',
+    androidScheme: 'https',
+  },
 };
 
 export default config;
