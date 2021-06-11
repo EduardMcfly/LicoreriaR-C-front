d798cd0b90c743362c829cc63620ab1f5d330838
Eduard Castellanos <andrescastellanos112013@gmail.com>
2021-06-11 12:08:46 -0500

    chore(Cart/steps): DetailsOrder component is added

---
+++ src/components/Cart/steps/DetailsOrder/index.tsx
@@ -0,0 +1,77 @@
+import format from 'date-fns/format';
+import addDays from 'date-fns/addDays';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+
+import { useShop } from 'contexts';
+import parse from 'date-fns/parse';
+import { maxDaysOrder } from '../../constants';
+import { Typography } from '@material-ui/core';
+
+const dateFormat = 'yyyy-MM-dd';
+export const DetailsOrder = () => {
+  const { userInfo } = useShop();
+  const { name, orderDate, orderTime, onChange } = userInfo;
+
+  const getDate = (date: Date | number): string => {
+    try {
+      return format(date, dateFormat);
+    } catch (error) {
+      return getDate(new Date());
+    }
+  };
+  const date = getDate(orderDate);
+  const time = orderTime;
+  return (
+    <Grid>
+      <TextField
+        label="Nombre completo"
+        name="name"
+        value={name}
+        onChange={({ target: { value } }) => {
+          onChange({ name: value });
+        }}
+        margin="dense"
+        type="text"
+        autoFocus
+        fullWidth
+      />
+      <Typography gutterBottom variant="body2">
+        Datos del pedido
+      </Typography>
+      <TextField
+        label="Fecha"
+        name="date"
+        value={date}
+        onChange={({ target: { value } }) => {
+          const newValue = parse(value, dateFormat, new Date());
+          if (!isNaN(+newValue)) onChange({ orderDate: newValue });
+        }}
+        margin="dense"
+        type="date"
+        fullWidth
+        InputLabelProps={{ shrink: true }}
+        inputProps={{
+          max: getDate(addDays(new Date(), maxDaysOrder)),
+          min: getDate(new Date().setHours(0, 0, 0, 0)),
+        }}
+      />
+      <TextField
+        label="Hora"
+        name="hour"
+        value={time}
+        onChange={({ target: { value } }) => {
+          onChange({
+            orderTime: value,
+          });
+        }}
+        margin="dense"
+        type="time"
+        fullWidth
+        InputLabelProps={{ shrink: true }}
+      />
+    </Grid>
+  );
+};
+
+export default DetailsOrder;
