083c712dece5c0b867625369a72fbc5d43e9036d
Eduard Castellanos <andrescastellanos112013@gmail.com>
2021-06-11 23:10:38 -0500

    chore(Cart/steps/common): useOnBuy updated

--- src/components/Cart/steps/common.tsx
+++ src/components/Cart/steps/common.tsx
@@ -1,25 +1,50 @@
 import qs from 'querystring';
 
 import { useShop } from 'contexts';
+import { ProductOrderInput, useCreateOrder } from 'graphqlAPI';
 import { getUrlWhatsapp } from 'utils';
+import { createRouteOrder } from '../../../constantsApp/index';
+import { ApolloError } from '@apollo/client';
 
-export const useOnBuy = () => {
-  const { products, removeProducts } = useShop();
-  return () => {
+type OnBuy = [
+  () => Promise<void>,
+  {
+    loading: boolean;
+    error?: ApolloError;
+  },
+];
+
+export const useOnBuy = (): OnBuy => {
+  const { products, removeProducts, map, userInfo } = useShop();
+  const [createOrder, { loading, error }] = useCreateOrder();
+  const handler = async () => {
+    const { center } = map;
+    const { name, orderDate } = userInfo;
+    if (!center || !orderDate || !name) return;
+    const { lat, lng } = center;
+
+    const { data, errors } = await createOrder({
+      variables: {
+        products: products.map<ProductOrderInput>(
+          ({ id, amount }) => ({ id, amount }),
+        ),
+        client: name,
+        location: { lat, lng },
+        orderDate,
+      },
+    });
+    const order = data?.createOrder;
+    if (!order || errors) throw new Error('Error creating order');
+    const { origin } = window.location;
     let text = `Hola\n`;
-    text += `Estoy interad@ en comprar estos productos:\n`;
-    for (const { id, amount } of products) {
-      const cartProduct = products.find((x) => x.id === id);
-      if (cartProduct) {
-        const { product } = cartProduct;
-        text += `${product.name}: ${amount}\n`;
-      }
-    }
+    text += `Esta es mi orden de compra:\n`;
+    text += origin + createRouteOrder({ id: order.id });
     let url = getUrlWhatsapp();
     url += qs.stringify({ text });
     window.open(url, '_blank')?.focus();
     removeProducts();
   };
+  return [handler, { loading, error }];
 };
 
 export enum Action {
