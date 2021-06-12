7c5e81e4f829708d7cc8a0cf1ecb8d461a0ece65
Eduard Castellanos <andrescastellanos112013@gmail.com>
2021-06-11 23:09:30 -0500

    fix(graphqlAPI/order): createOrder mutation updated

--- src/graphqlAPI/order/mutation.ts
+++ src/graphqlAPI/order/mutation.ts
@@ -4,11 +4,13 @@ import { Mutation, MutationCreateOrderArgs } from '../types-graphql';
 export const CREATE_ORDER_MUTATION = gql`
   mutation (
     $orderDate: DateTime!
+    $client: String!
     $location: OrderLocationInput!
     $products: [ProductOrderInput!]!
   ) {
     createOrder(
       orderDate: $orderDate
+      client: $client
       location: $location
       products: $products
     ) {
