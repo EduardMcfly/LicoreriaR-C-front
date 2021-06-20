c287aced9efb5c80f66021f185db81c390c09d57
Eduard Castellanos <andrescastellanos112013@gmail.com>
2021-06-19 22:47:58 -0500

    chore(pages/order): useOrder query added

--- src/pages/Order/index.tsx
+++ src/pages/Order/index.tsx
@@ -1,11 +1,15 @@
 import { RouteComponentProps } from 'react-router-dom';
 
+import { useOrder } from 'graphqlAPI';
+
 type OrderProps = RouteComponentProps<{
   id: string;
 }>;
 
 export const Order = ({ match }: OrderProps) => {
   const { id } = match.params;
+  const { data } = useOrder({ variables: { id } });
+  console.log(data);
 
   return <div>{id}</div>;
 };
