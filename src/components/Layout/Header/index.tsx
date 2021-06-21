db929206b4b9b5155c6e93095ad5b7a19a1e3709
Eduard Castellanos <andrescastellanos112013@gmail.com>
2021-06-21 18:31:35 -0500

    chore(Layout/Header): Component has been improved

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -4,6 +4,8 @@ import {
   makeStyles,
   Typography,
   Avatar,
+  AppBarProps,
+  ToolbarProps,
 } from '@material-ui/core';
 
 import { AppName } from 'constantsApp';
@@ -18,15 +20,22 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export const Header = () => {
+interface HeaderProps {
+  items?: boolean;
+  appBarProps?: AppBarProps;
+  toolbarProps?: ToolbarProps;
+}
+
+export const Header = (props?: HeaderProps) => {
   const classes = useStyles();
+  const { items = true, appBarProps, toolbarProps } = { ...props };
   return (
     <>
-      <AppBar>
-        <Toolbar variant="regular">
+      <AppBar {...appBarProps}>
+        <Toolbar variant="regular" {...toolbarProps}>
           <Avatar src={logo} className={classes.icon} />
           <Typography variant="h5">{AppName}</Typography>
-          <Items />
+          {items && <Items />}
         </Toolbar>
       </AppBar>
       <div className={classes.appBarSpacer} />
