5055782c31d60d9e5fa8c8d77c2e969418f41020
Eduard Castellanos <andrescastellanos112013@gmail.com>
2021-06-22 20:58:45 -0500

    chore(components): Has been added the component Map

---
+++ src/components/Map/index.tsx
@@ -0,0 +1,40 @@
+import GoogleMapReact, { Props } from 'google-map-react';
+import { makeStyles } from '@material-ui/core';
+
+import { useShop } from 'contexts';
+import { Marker } from './Marker';
+import clsx from 'clsx';
+
+const { REACT_APP_MAPS } = process.env;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: `calc(100vh - ${theme.spacing(50)}px)`,
+    width: '100%',
+    minHeight: theme.spacing(40),
+  },
+}));
+
+type MapProps = Props & {
+  className?: string;
+};
+
+const Map = (props: MapProps) => {
+  const classes = useStyles();
+  const { className, center, defaultZoom, ...rest } = props;
+  return (
+    <div className={clsx(classes.root, className)}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: REACT_APP_MAPS }}
+        defaultZoom={defaultZoom || 12}
+        center={center}
+        options={{ gestureHandling: 'greedy' }}
+        {...rest}
+      >
+        {center && <Marker lat={center.lat} lng={center.lng} />}
+      </GoogleMapReact>
+    </div>
+  );
+};
+
+export default Map;
