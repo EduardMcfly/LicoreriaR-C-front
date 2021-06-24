fe97cf9d160bfc3a44d347e9cd6eb836d999463c
Eduard Castellanos <andrescastellanos112013@gmail.com>
2021-06-24 07:35:46 -0500

    chore(pages/Order): Component DeliveryInformation is added

---
+++ src/pages/Order/DeliveryInformation.tsx
@@ -0,0 +1,75 @@
+import {
+  makeStyles,
+  Typography,
+  Grid,
+  Divider,
+} from '@material-ui/core';
+import format from 'date-fns/format';
+import parseISO from 'date-fns/parseISO';
+import differenceInMinutes from 'date-fns/differenceInMinutes';
+
+const useStyles = makeStyles((theme) => ({
+  root: { padding: theme.spacing(1, 1, 2) },
+}));
+
+interface OrderDateProps {
+  deliveryDate?: string;
+  orderDate?: string;
+}
+
+export const DeliveryInformation = ({
+  deliveryDate,
+  orderDate,
+}: OrderDateProps) => {
+  const classes = useStyles();
+  const dateParsed = deliveryDate && parseISO(deliveryDate);
+  const orderDateParsed = orderDate && parseISO(orderDate);
+
+  const dateTime = dateParsed && format(dateParsed, 'dd-MM-Y H:mm');
+
+  const deliveryDifference =
+    !dateTime &&
+    orderDateParsed &&
+    differenceInMinutes(new Date(), orderDateParsed);
+
+  const deliveryCanceled =
+    deliveryDifference && deliveryDifference > 60;
+
+  const deliveryDelayed =
+    !deliveryCanceled && deliveryDifference && deliveryDifference > 1;
+
+  const hasProblem = deliveryDelayed || deliveryCanceled;
+
+  return (
+    <Grid item xs={12}>
+      <Grid container className={classes.root} alignItems="center">
+        <Grid item xs={12}>
+          <Typography
+            color="primary"
+            variant="subtitle2"
+            align="center"
+            gutterBottom
+          >
+            Información de entrega
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          {dateTime && (
+            <Typography color="primary" variant="h6" align="center">
+              Entregado el {dateTime}
+            </Typography>
+          )}
+          {hasProblem && (
+            <Typography color="error" variant="h6" align="center">
+              {deliveryCanceled && 'La entrega se cancelo'}
+              {deliveryDelayed && 'La entrega se retrasó'}
+            </Typography>
+          )}
+        </Grid>
+      </Grid>
+      <Divider />
+    </Grid>
+  );
+};
+
+export default DeliveryInformation;
