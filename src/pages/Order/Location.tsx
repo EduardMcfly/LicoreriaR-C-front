cd2d2dccb4b15cd74887d3cbf31aa144b23a0329
Eduard Castellanos <andrescastellanos112013@gmail.com>
2021-06-24 07:36:36 -0500

    chore(pages/Order): Component Location is added

---
+++ src/pages/Order/Location.tsx
@@ -0,0 +1,27 @@
+import { makeStyles } from '@material-ui/core';
+
+import { OrderLocation } from 'graphqlAPI';
+import Map from 'components/Map';
+
+const useStyles = makeStyles((theme) => ({
+  map: {
+    height: theme.spacing(40),
+    width: '100%',
+    minHeight: theme.spacing(40),
+  },
+}));
+
+interface LocationProps {
+  location: OrderLocation;
+}
+
+const Location = ({ location }: LocationProps) => {
+  const classes = useStyles();
+  const { lat, lng } = location;
+
+  return (
+    <Map center={{ lat, lng }} zoom={18} className={classes.map} />
+  );
+};
+
+export default Location;
